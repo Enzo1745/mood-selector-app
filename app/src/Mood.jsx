c472ae1772c6cc6bdc9f36265d7462c56e0ddecf
eperei03 <eperei03@etudiant.univ-lr.fr>
2025-01-28 19:31:44 +0100

    [feat](Mood display): different messages and colors depending on users emotions

--- app/src/Mood.jsx
+++ app/src/Mood.jsx
@@ -4,15 +4,36 @@ import Emoji from "./Emoji.jsx";
 function Mood() {
 
     const [mood, setMood] = useState("none");
+    const [currentMoodMessage, setCurrentMoodMessage] = useState("How do you feel today?");
+    const [currentMoodDisplay, setcurrentMoodDisplay] = useState("");
+
+    const displayMood = (mood) => {
+
+        setMood(mood);
+
+        // Displays a message depending on users mood
+        if(mood === "Happy") {
+            setCurrentMoodMessage("Yay! You feel happy today!");
+            setcurrentMoodDisplay("positive-emotion");
+        }
+        else if(mood === "Sad") {
+            setCurrentMoodMessage("Don't feel sad, go out with your friends!");
+            setcurrentMoodDisplay("negative-emotion");
+        }
+        else if(mood === "Angry") {
+            setCurrentMoodMessage("Don't be angry, have a warm cup of tea!");
+            setcurrentMoodDisplay("negative-emotion");
+        }
+    }
 
     return(
-        <div className="container">
-            <h3>How do you feel today?</h3>
+        <div className="container" id={currentMoodDisplay}>
+            <h3>{currentMoodMessage}</h3>
             <label className="mood-message">Select your mood: </label>
             <select className="select-button"
             value={mood}
-            onChange={(e) => setMood(e.target.value)}>
-                <option value=""></option>
+            onChange={(element) => displayMood(element.target.value)}>
+                {mood === "none" && <option value=""></option>}
                 <option value="Happy">Happy</option>
                 <option value="Sad">Sad</option>
                 <option value="Angry" >Angry</option>
